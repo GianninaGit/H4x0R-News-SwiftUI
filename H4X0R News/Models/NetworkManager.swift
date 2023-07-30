11031c73ac8759644aa308771d28829e145b505f
Giannina <giannisarappa@gmail.com>
2023-07-31 00:27:37 +0200

    Networking in SwiftUI - Video 215

---
+++ H4X0R News/Models/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  H4X0R News
+//
+//  Created by Giannina on 30/7/23.
+//
+
+import Foundation
---
+++ H4X0R News/Models/PostData.swift
@@ -0,0 +1,8 @@
+//
+//  PostData.swift
+//  H4X0R News
+//
+//  Created by Giannina on 30/7/23.
+//
+
+import Foundation
